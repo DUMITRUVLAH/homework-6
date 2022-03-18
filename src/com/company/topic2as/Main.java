a96268b7b0e2d677a32be29658e7ff3268f3958c
andrei.secu <andrei.secu@amdaris.com>
2022-03-18 18:51:47 +0200

    Add the code related to Variables and Access Modifiers

---
+++ src/com/company/topic2as/Main.java
@@ -0,0 +1,11 @@
+package com.company.topic2as;
+
+public class Main {
+    public static void main(String[] args) {
+        Persoana marinelPersona = new Persoana();
+
+        marinelPersona.numePersoana = "Marin";
+        marinelPersona.seteazaVarsta(90);
+        System.out.println(marinelPersona.getVirsta());
+    }
+}
